6f913a7cec70c7ef57f91ad976f1043d30293a01
Partha Mandal <43367843+partham16@users.noreply.github.com>
2020-09-26 04:50:37 +0530

    Create main.py

---
+++ python __mro__/main.py
@@ -0,0 +1,42 @@
+class A: ...
+
+class A1(A): ...
+class A2(A): ...
+
+class B11(A1): ...
+class B12(A1): ...
+
+class B21(A2): ...
+
+class D1(B11,B12): ...
+class D2(B11,B21): ...
+
+print("A->A1->B11, B12 | A->A2->B21")
+print("B11, B12 -> D1 | B11, B21 -> D2 \n")
+print(D1.__mro__)
+print("")
+print(D2.__mro__)
+
+print("\n^^MRO goes upwards, but, subclass always before parent \n")
+
+"""ABC v/s ABCMeta in __mro__"""
+
+from abc import ABC, abstractmethod
+from abc import ABCMeta
+
+class Animal(ABC):
+
+    @abstractmethod
+    def move(self):
+        pass
+
+class Shape(metaclass=ABCMeta):
+    def __init__(self, id):
+        self._id = id
+
+    @abstractmethod
+    def display(self): 
+        pass
+
+print(Shape.__class__.__mro__)
+print(Animal.__class__.__mro__)
